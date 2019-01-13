39c8e9da89c3f6def3b3771ed8f94c01d022e6ab
Medi Assumani <mediassumani49@gmail.com>
2019-01-12 21:38:50 -0800

    starter code for reddit clone

---
+++ Jeddit/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+      mongoose = require("mongoose");
+      bodyParser = require("body-parser");
+      methodOverride = require("method-override");
+      exphbs = require("express-handlebars");
+      http = require("http");
+      port = process.env.PORT || 3000;
+      app = express():
+
+
+
+
+app.listen(port);
