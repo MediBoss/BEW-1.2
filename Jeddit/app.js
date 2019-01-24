dce5d436ef280f73034f4d20fd3ce3b82964a9b6
Medi Assumani <mediassumani49@gmail.com>
2019-01-24 09:44:56 -0800

    [Add] hello jeddit

--- Jeddit/app.js
+++ Jeddit/app.js
@@ -1,13 +1,24 @@
-const express = require("express");
-      mongoose = require("mongoose");
-      bodyParser = require("body-parser");
-      methodOverride = require("method-override");
-      exphbs = require("express-handlebars");
-      http = require("http");
-      port = process.env.PORT || 3000;
-      app = express():
+// LOADING NEEDED MODULES
+const express = require("express")
+      mongoose = require("mongoose")
+      bodyParser = require("body-parser")
+      methodOverride = require("method-override")
+      exphbs = require("express-handlebars")
+      http = require("http")
+      port = process.env.PORT || 3000
+      app = express()
+
+
+// SETTING UP VIEWS AND MIDDLEWARE
+app.engine("handlebars", exphbs({ defaultLayout: 'main' }))
+app.set("view engine", "handlebars")
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", (request, response) => {
+  console.log("Hellow word")
+})
 
 
 
-
-app.listen(port);
+// SERVER BOOTING UP
+app.listen(port)
